79f845e2c7aefcd6bcab432075d3f996aeb55068
Diego Fernandes <diego.schell.f@gmail.com>
2019-07-12 11:23:31 -0300

    2 - Configurando Root Import

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,5 +32,12 @@ module.exports = {
     "no-console": ["error", { allow: ["tron"] }],
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
-  }
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      }
+    }
+  },
 };
