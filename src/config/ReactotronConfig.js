8b787525224886de355c383e5301d4fcd6d73f3b
Diego Fernandes <diego.schell.f@gmail.com>
2019-07-12 18:37:16 -0300

    10 - Reactotron

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,15 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
+
+if (__DEV__) {
+  const tron = Reactotron.configure()
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+
+// import './config/ReactotronConfig';
+
 import Routes from './routes';
 
 export default function App() {
