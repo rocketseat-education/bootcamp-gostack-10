6d7f045d7a0c48be4f4f3c4f1f9c8324cbc290ea
Diego Fernandes <diego.schell.f@gmail.com>
2019-07-12 17:21:34 -0300

    4 - Criando Background

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <View />;
+  return (
+    <Background>
+      <Text>SignUp</Text>
+    </Background>
+  );
 }
