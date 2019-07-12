cfa13784ef851f6073c9c1bb019357be01556ed9
Diego Fernandes <diego.schell.f@gmail.com>
2019-07-12 18:18:11 -0300

    8 - Configurando StatusBar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
